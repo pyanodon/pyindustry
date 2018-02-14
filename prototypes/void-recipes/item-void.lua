b70ef192150a164663fe37a199f71336124d6df0
Nicholas Dunn <nexela@gmail.com>
2018-02-13 23:03:57 -0500

    Fixed burner stuffs
    Former-commit-id: 6b3323e093f88d79c68281f1770d483108709a57
    Former-commit-id: 0cc0b320d4129d5f7c241f4f5bf490f3b72e96bd

--- prototypes/void-recipes/item-void.lua
+++ prototypes/void-recipes/item-void.lua
@@ -1,10 +1,22 @@
 -- Courtesy of "pyindustry_burner_fix", by "magu5026",
 
+local void_result = {
+    type = "item",
+    name = Item("ash"):valid() and "ash" or Item("coal"):valid() and "coal" or nil,
+    amount_min = 1,
+    amount_max = 1,
+    probability = 0.20
+}
+if not void_result.name then
+    void_result = {type = "fluid", name = "water", amount = 0}
+end
+
 local function make_void_recipe(name, newicons, ing_name)
     Recipe {
         name = name,
         type = "recipe",
         localised_name = {"", "Void ", {"item-name." .. ing_name}},
+        energy_required = 1.5,
         icons = newicons,
         icon_size = 32,
         hidden = true,
@@ -12,22 +24,24 @@ local function make_void_recipe(name, newicons, ing_name)
         flags = {"hidden"},
         category = "py-incineration",
         ingredients = {{type = "item", name = ing_name, amount = 1}},
-        results = {{type = "fluid", name = "water", amount = 0}},
+        results = {void_result},
         subgroup = "py-void-items"
     }
 end
 
 for _, type in pairs(Item.item_types) do
     for _, item in pairs(data.raw[type]) do
-        local name = item.name .. "-pyvoid"
+        if not item.fuel_value then
+            local name = item.name .. "-pyvoid"
 
-        local new_icons
-        if item.icons then
-            new_icons = table.deepcopy(item.icons)
-        else
-            new_icons = {{icon = item.icon}}
+            local new_icons
+            if item.icons then
+                new_icons = table.deepcopy(item.icons)
+            else
+                new_icons = {{icon = item.icon}}
+            end
+            new_icons[#new_icons + 1] = {icon = "__pyindustry__/graphics/icons/no.png"}
+            make_void_recipe(name, new_icons, item.name)
         end
-        new_icons[#new_icons + 1] = {icon = "__pyindustry__/graphics/icons/no.png"}
-        make_void_recipe(name, new_icons, item.name)
     end
 end
