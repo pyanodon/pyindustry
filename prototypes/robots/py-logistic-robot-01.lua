2b504b635d65684a4ac055474c1319e9ea427d88
Nicholas Dunn <nexela@gmail.com>
2018-03-25 22:07:56 -0400

    Many Changes
    Former-commit-id: 8c403e9a4641c8abc07c627cf0da1435dc7dc202
    Former-commit-id: 4f8f1bca1519365ba14fcb1b4022c3e091795085

--- prototypes/robots/py-logistic-robot-01.lua
+++ prototypes/robots/py-logistic-robot-01.lua
@@ -1,7 +1,7 @@
 local cargo_bot =
     RECIPE {
     type = 'recipe',
-    name = 'cargo-01',
+    name = 'py-logistic-robot-01',
     energy_required = 10,
     category = 'crafting-with-fluid',
     enabled = false,
@@ -10,11 +10,11 @@ local cargo_bot =
         {'iron-gear-wheel', 10},
         {'copper-plate', 2},
         {'advanced-circuit', 10},
-        {'cr-01', 1},
+        {'py-construction-robot-01', 1},
         {type = 'fluid', name = 'lubricant', amount = 300}
     },
     results = {
-        {'cargo-01', 1}
+        {'py-logistic-robot-01', 1}
     }
 }
 cargo_bot:add_ingredient({type = 'item', name = 'rubber', amount = 10})
@@ -23,23 +23,23 @@ cargo_bot:add_unlock({'basic-electronics', 'advanced-electronics'})
 
 ITEM {
     type = 'item',
-    name = 'cargo-01',
-    icon = '__pyindustry__/graphics/icons/cargo-01.png',
+    name = 'py-logistic-robot-01',
+    icon = '__pyindustry__/graphics/icons/py-logistic-robot-01.png',
     icon_size = 32,
     flags = {'goes-to-quickbar'},
     subgroup = 'py-robots',
     order = 'c',
-    place_result = 'cargo-01',
+    place_result = 'py-logistic-robot-01',
     stack_size = 10
 }:subgroup_order('py-hightech-items', 'b')
 
 ENTITY {
     type = 'logistic-robot',
-    name = 'cargo-01',
-    icon = '__pyindustry__/graphics/icons/cargo-01.png',
+    name = 'py-logistic-robot-01',
+    icon = '__pyindustry__/graphics/icons/py-logistic-robot-01.png',
     icon_size = 32,
     flags = {'placeable-player', 'player-creation', 'placeable-off-grid', 'not-on-map'},
-    minable = {hardness = 0.1, mining_time = 0.1, result = 'cargo-01'},
+    minable = {hardness = 0.1, mining_time = 0.1, result = 'py-logistic-robot-01'},
     resistances = {{type = 'fire', percent = 85}},
     max_health = 100,
     collision_box = {{0, 0}, {0, 0}},
@@ -54,7 +54,7 @@ ENTITY {
     min_to_charge = 0.2,
     max_to_charge = 0.95,
     idle = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/idle.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/idle.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -64,7 +64,7 @@ ENTITY {
         direction_count = 16,
         --y = 42,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-idle.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-idle.png',
             priority = 'high',
             line_length = 16,
             width = 128,
@@ -77,7 +77,7 @@ ENTITY {
         }
     },
     idle_with_cargo = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/idle-cargo.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/idle-cargo.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -86,7 +86,7 @@ ENTITY {
         shift = util.by_pixel(0, 0),
         direction_count = 16,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-idle-cargo.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-idle-cargo.png',
             priority = 'high',
             line_length = 16,
             width = 128,
@@ -98,7 +98,7 @@ ENTITY {
         }
     },
     in_motion = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/run.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/run.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -107,7 +107,7 @@ ENTITY {
         shift = util.by_pixel(0, 0),
         direction_count = 16,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-run.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-run.png',
             priority = 'high',
             line_length = 16,
             width = 128,
@@ -119,7 +119,7 @@ ENTITY {
         }
     },
     in_motion_with_cargo = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/run-cargo.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/run-cargo.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -128,7 +128,7 @@ ENTITY {
         shift = util.by_pixel(0, 0),
         direction_count = 16,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-run-cargo.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-run-cargo.png',
             priority = 'high',
             line_length = 16,
             width = 128,
@@ -140,7 +140,7 @@ ENTITY {
         }
     },
     shadow_idle = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/shadow.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/shadow.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -150,7 +150,7 @@ ENTITY {
         draw_as_shadow = true,
         direction_count = 16,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-shadow.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-shadow.png',
             priority = 'high',
             line_length = 16,
             width = 128,
@@ -163,7 +163,7 @@ ENTITY {
         }
     },
     shadow_idle_with_cargo = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/shadow-cargo.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/shadow-cargo.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -173,7 +173,7 @@ ENTITY {
         draw_as_shadow = true,
         direction_count = 16,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-shadow-cargo.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-shadow-cargo.png',
             priority = 'high',
             line_length = 16,
             width = 128,
@@ -186,7 +186,7 @@ ENTITY {
         }
     },
     shadow_in_motion = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/shadow.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/shadow.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -196,7 +196,7 @@ ENTITY {
         draw_as_shadow = true,
         direction_count = 16,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-shadow.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-shadow.png',
             priority = 'high',
             line_length = 16,
             width = 128,
@@ -209,7 +209,7 @@ ENTITY {
         }
     },
     shadow_in_motion_with_cargo = {
-        filename = '__pyindustry__/graphics/entity/cargo-01/shadow-cargo.png',
+        filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/shadow-cargo.png',
         priority = 'high',
         line_length = 16,
         width = 64,
@@ -219,7 +219,7 @@ ENTITY {
         draw_as_shadow = true,
         direction_count = 16,
         hr_version = {
-            filename = '__pyindustry__/graphics/entity/cargo-01/hr-shadow-cargo.png',
+            filename = '__pyindustry__/graphics/entity/py-logistic-robot-01/hr-shadow-cargo.png',
             priority = 'high',
             line_length = 16,
             width = 128,
