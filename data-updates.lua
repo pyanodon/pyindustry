03fb17b6c6f71f53d6913d5ff6a77aa2075bb9d0
pyanodon <smylodon@gmail.com>
2018-04-07 20:11:07 -0300

    added data-updates

---
+++ data-updates.lua
@@ -0,0 +1,1 @@
+TECHNOLOGY('logistic-system'):remove_pack('high-tech-science-pack'):remove_pack('production-science-pack'):remove_pack('science-pack-3')--:execute(function(self) log(serpent.block(self.unit.ingredients)) error() end)
