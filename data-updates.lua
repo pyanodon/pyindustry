10d09d96f1e83c528b6e7a687bbb4bf653f91d9c
pyanodon <smylodon@gmail.com>
2018-10-11 19:20:11 -0300

    updates

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,6 @@
 TECHNOLOGY('logistic-system'):remove_pack('high-tech-science-pack'):remove_pack('production-science-pack'):remove_pack('science-pack-3')
 RECIPE('logistic-chest-storage'):remove_ingredient('advanced-circuit')
+RECIPE('logistic-chest-passive-provider'):remove_ingredient('advanced-circuit')
+RECIPE('logistic-chest-active-provider'):remove_ingredient('advanced-circuit')
+RECIPE('logistic-chest-buffer'):remove_ingredient('advanced-circuit')
+RECIPE('logistic-chest-requester'):remove_ingredient('advanced-circuit')
