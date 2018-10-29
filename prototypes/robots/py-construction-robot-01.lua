2067b26695ed23710d735692b8e188b6734084e9
pyanodon <smylodon@gmail.com>
2018-10-28 22:02:25 -0300

    updates
    Former-commit-id: f0fc3b9f9b01dd2b2affdfc4a39bd7c9c4d2d7c8
    Former-commit-id: 0bc79d4ffd1b9062a3c37bc6deb5da1569d735cd

--- prototypes/robots/py-construction-robot-01.lua
+++ prototypes/robots/py-construction-robot-01.lua
@@ -39,7 +39,7 @@ ENTITY {
     collision_box = {{0, 0}, {0, 0}},
     selection_box = {{-0.5, -1.5}, {0.5, -0.5}},
     max_payload_size = 1,
-    speed = 0.06,
+    speed = 0.08,
     transfer_distance = 0.5,
     max_energy = '1.0MJ',
     energy_per_tick = '0.05kJ',
