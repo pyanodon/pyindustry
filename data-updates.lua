8e7211dcc2adb5feca0a3d1c4d178e954837941c
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-03-25 04:55:41 -0400

    Night of the living modules
    added all pyid recipes names to list to have productivity modules

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,67 @@
+local FUN = require("__pycoalprocessing__/prototypes/functions/functions")
+
 TECHNOLOGY('logistic-system'):remove_pack('high-tech-science-pack'):remove_pack('production-science-pack'):remove_pack('science-pack-3'):remove_prereq('advanced-electronics')
 RECIPE('logistic-chest-storage'):remove_ingredient('advanced-circuit')
 RECIPE('logistic-chest-passive-provider'):remove_ingredient('advanced-circuit')
 RECIPE('logistic-chest-active-provider'):remove_ingredient('advanced-circuit')
 RECIPE('logistic-chest-buffer'):remove_ingredient('advanced-circuit')
 RECIPE('logistic-chest-requester'):remove_ingredient('advanced-circuit')
+
+local recipes_list =
+{
+  "niobium-pipe",
+  "niobium-pipe-to-ground",
+  "py-asphalt",
+  "py-limestone",
+  "py-coal-tile",
+  "py-iron",
+  "py-steel",
+  "py-aluminium",
+  "py-iron-oxide",
+  "py-nexelit",
+  "poorman-wood-fence",
+  "wood-fence",
+  "concrete-wall",
+  "py-shed-basic",
+  "py-shed-passive-provider",
+  "py-shed-storage",
+  "py-shed-active-provider",
+  "py-shed-requester",
+  "py-shed-buffer",
+  "py-storehouse-basic",
+  "py-storehouse-passive-provider",
+  "py-storehouse-storage",
+  "py-storehouse-active-provider",
+  "py-storehouse-requester",
+  "py-storehouse-buffer",
+  "py-warehouse-basic",
+  "py-warehouse-passive-provider",
+  "py-warehouse-storage",
+  "py-warehouse-active-provider",
+  "py-warehouse-requester",
+  "py-warehouse-buffer",
+  "py-tank-1000",
+  "py-tank-1500",
+  "py-tank-3000",
+  "py-tank-4000",
+  "py-tank-5000",
+  "py-tank-7000",
+  "py-tank-6500",
+  "py-tank-8000",
+  "py-check-valve",
+  "py-overflow-valve",
+  "py-underflow-valve",
+  "accumulator-mk01",
+  "accumulator-mk02",
+  "py-local-radar",
+  "py-roboport-mk01",
+  "py-recharge-station-mk01",
+  "py-construction-robot-01",
+  "py-logistic-robot-01",
+  "py-burner",
+  "py-sinkhole",
+  "py-gas-vent"
+}
+
+--adding to module limitation list
+FUN.productivity(recipes_list)
