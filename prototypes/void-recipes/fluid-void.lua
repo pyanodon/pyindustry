57d0bc9bdccd6b217109c4bbc97e36d506588935
Fabboz <fabuz.135@gmail.com>
2019-06-04 21:42:20 +0200

    Presumible definetly void recipes  fix

--- prototypes/void-recipes/fluid-void.lua
+++ prototypes/void-recipes/fluid-void.lua
@@ -38,7 +38,7 @@ for _, fluid in pairs(data.raw.fluid) do
         local locale = {"", "Void ", {"fluid-name." .. fluid.name}}
         local ing = {type = "fluid", name = fluid.name, maximum_temperature = fluid.gas_temperature, amount = 10}
         make_fluid_recipe(name, locale, icons, "py-runoff", ing, "py-void-liquid")
-	else if fluid.name ~= "steam" then
+	elseif fluid.name ~= "steam" then
 		if fluid.gas_temperature then
 			--Make venting
 			name = fluid.name .. '-pyvoid-gas'
@@ -47,5 +47,4 @@ for _, fluid in pairs(data.raw.fluid) do
 			make_fluid_recipe(name, locale, icons, "py-venting", ing, "py-void-gas")
 		end
 	end
-end
 end
