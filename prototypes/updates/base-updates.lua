4f0981fd83283708e08f0cd874bb82be299faa4c
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-10-18 19:06:13 -0400

    create factoriotest.yml

--- prototypes/updates/base-updates.lua
+++ prototypes/updates/base-updates.lua
@@ -1,11 +1,13 @@
 
 data.raw.fluid['petroleum-gas'].gas_temperature = 20
 
+if not mods['boblogistics'] then
 --Remove base robots
 RECIPE('construction-robot'):remove_unlock('construction-robotics')
 RECIPE('logistic-robot'):remove_unlock('logistic-robotics')
 RECIPE('flying-robot-frame'):remove_unlock('robotics')
 RECIPE("roboport"):remove_unlock('logistic-robotics')
+end
 
 --move base techs around to support pynobots
 TECHNOLOGY('logistic-system'):remove_pack('utility-science-pack'):remove_pack('production-science-pack'):remove_pack('chemical-science-pack'):remove_prereq('utility-science-pack')
