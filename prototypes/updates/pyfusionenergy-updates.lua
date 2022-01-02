37ad2a6862fe18f60d9d189aa277f507f41883b1
pyanodon <smylodon@gmail.com>
2022-01-02 13:52:08 -0300

    heat exchanger

--- prototypes/updates/pyfusionenergy-updates.lua
+++ prototypes/updates/pyfusionenergy-updates.lua
@@ -8,4 +8,7 @@ TECHNOLOGY('fluid-processing-machines-1'):add_prereq('py-storage-tanks')
 
 -- RECIPE CHANGES
 RECIPE("mixer-mk01"):replace_ingredient("storage-tank", "py-tank-8000")
-RECIPE("py-heat-exchanger"):replace_ingredient("storage-tank", "py-tank-8000")
+RECIPE("py-heat-exchanger"):replace_ingredient("storage-tank", "py-tank-7000")
+RECIPE("py-heat-exchanger-mk02"):add_ingredient({type = "item", name = "py-tank-8000", amount = 1})
+RECIPE("py-heat-exchanger-mk03"):add_ingredient({type = "item", name = "py-tank-9000", amount = 1})
+RECIPE("py-heat-exchanger-mk04"):add_ingredient({type = "item", name = "py-tank-10000", amount = 1})
