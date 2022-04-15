360bca0f7de2c600731bcbff54da664198c4cde7
ShadowGlass0 <gkecskem@gmail.com>
2022-04-15 11:43:15 +1000

    railway-mk03

--- prototypes/technologies/railway.lua
+++ prototypes/technologies/railway.lua
@@ -38,6 +38,28 @@ if mods['pycoalprocessing'] then
     }
 end
 
+if mods["pyhightech"] then
+    TECHNOLOGY {
+        type = "technology",
+        name = "railway-mk03",
+        icon = "__pyindustry__/graphics/technology/railway-mk03.png",
+        icon_size = 128,
+        order = "c-a",
+        prerequisites = {"railway-mk02"},
+        dependencies = {"railway-mk02"},
+        effects = {},
+        unit = {
+            count = 250,
+            ingredients = {
+                {"automation-science-pack", 1},
+                {"logistic-science-pack", 1},
+                {"chemical-science-pack", 1}
+            },
+            time = 45
+        }
+    }
+end
+
 if mods['pyalternativeenergy'] then
 
     TECHNOLOGY {
