0ceb616137fd35ea15b828692c4bd040597692b8
ShadowGlass0 <gkecskem@gmail.com>
2022-05-12 01:36:23 +1000

    Accumulators mk03 (#63)

--- prototypes/technologies/accumulators.lua
+++ prototypes/technologies/accumulators.lua
@@ -39,3 +39,28 @@ TECHNOLOGY {
     order = "c-k-a",
     upgrade = true
 }
+
+if mods["pyalternativeenergy"] then
+    TECHNOLOGY {
+        type = "technology",
+        name = "py-accumulator-mk03",
+        icon = "__pyindustry__/graphics/technology/accumulator-mk03.png",
+        icon_size = 128,
+        effects = {},
+        prerequisites = {"production-science-pack", "py-accumulator-mk01"},
+        dependencies = {"py-accumulator-mk02"},
+        unit = {
+            count = 200,
+            ingredients = {
+                {"automation-science-pack", 2},
+                {"logistic-science-pack", 2},
+                {"chemical-science-pack", 2},
+                {"production-science-pack", 1},
+                {"utility-science-pack", 1},
+            },
+            time = 60
+        },
+        order = "c-k-a",
+        upgrade = true
+    }
+end
