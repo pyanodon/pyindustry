5ca80929ba021f248095e18d0d7feac3203dcfb2
Sm^HpJk4 <zacharypicco@gmail.com>
2022-12-31 23:42:59 -0600

    increase armor inventory size

--- prototypes/equipment/armor-updates.lua
+++ prototypes/equipment/armor-updates.lua
@@ -16,3 +16,9 @@ data:extend{{
 
 data.raw.armor['light-armor'].equipment_grid = 'light-armor-equipment-grid'
 data.raw.armor['heavy-armor'].equipment_grid = 'heavy-armor-equipment-grid'
+
+data.raw.armor['light-armor'].inventory_size_bonus = 20
+data.raw.armor['heavy-armor'].inventory_size_bonus = 40
+data.raw.armor['modular-armor'].inventory_size_bonus = 60
+data.raw.armor['power-armor'].inventory_size_bonus = 80
+data.raw.armor['power-armor-mk2'].inventory_size_bonus = 100
