5c0f6ccdc2104ef33efd45d7cd9aa33afda92c36
Jake Staberg Morgan <47102583+JStMorgan@users.noreply.github.com>
2023-09-27 18:48:43 +0200

    Redefined Asphalt and Coal tile entities (#90)

--- prototypes/tiles/py-asphalt.lua
+++ prototypes/tiles/py-asphalt.lua
@@ -25,20 +25,45 @@ ITEM {
     order = 'zb',
     stack_size = 1000,
     default_request_amount = 150,
-    place_as_tile = {result = 'lab-dark-2', condition_size = 1, condition = {'water-tile'}}
+    place_as_tile = {result = 'py-asphalt', condition_size = 1, condition = {'water-tile'}}
 }
 
-data.raw.tile['lab-dark-2'].minable = {mining_time = 0.1, result = 'py-asphalt'}
-data.raw.tile['lab-dark-2'].mined_sound = {filename = '__base__/sound/deconstruct-bricks.ogg'}
-data.raw.tile['lab-dark-2'].walking_speed_modifier = 3.25
-data.raw.tile['lab-dark-2'].decorative_removal_probability = 1
-data.raw.tile['lab-dark-2'].layer = 100
-data.raw.tile['lab-dark-2'].walking_sound = table.deepcopy(data.raw.tile['grass-1'].walking_sound)
-data.raw.tile['lab-dark-2'].collision_mask = {'ground-tile'}
-data.raw.tile['lab-dark-2'].vehicle_friction_modifier = 0.6
-data.raw.tile['lab-dark-2'].tint = {0.4,0.4,0.4}
-
-local old = data.raw.tile['lab-dark-2'].variants.main
-add_material_background(data.raw.tile['lab-dark-2'])
-data.raw.tile['lab-dark-2'].variants.material_background = nil
-data.raw.tile['lab-dark-2'].variants.main = old
+ENTITY {
+    type = "tile",
+    name = "py-asphalt",
+    needs_correction = false,
+    minable = {mining_time = 0.1, result = "py-asphalt"},
+    mined_sound = {filename = "__base__/sound/deconstruct-bricks.ogg"},
+    collision_mask = {"ground-tile"},
+    walking_speed_modifier = 3.25,
+    decorative_removal_probability = 1,
+    layer = 100,
+    variants = {
+        main = data.raw.tile['lab-dark-2'].variants.main ,
+        inner_corner = {
+            picture = "__pyindustry__/graphics/tiles/py-asphalt/concrete-inner-corner.png",
+            count = 8
+        },
+        outer_corner = {
+            picture = "__pyindustry__/graphics/tiles/py-asphalt/concrete-outer-corner.png",
+            count = 8
+        },
+        side = {
+            picture = "__pyindustry__/graphics/tiles/py-asphalt/concrete-side.png",
+            count = 8
+        },
+        u_transition = {
+            picture = "__pyindustry__/graphics/tiles/py-asphalt/concrete-u.png",
+            count = 8
+        },
+        o_transition = {
+            picture = "__pyindustry__/graphics/tiles/py-asphalt/concrete-o.png",
+            count = 1
+        }
+    },
+    walking_sound = table.deepcopy(data.raw.tile['grass-1'].walking_sound),
+    map_color = {r = 0, g = 0, b = 0, a = 1},
+    pollution_absorption_per_second = 0,
+    vehicle_friction_modifier = 0.6,
+    tint = {0.4,0.4,0.4}
+}
