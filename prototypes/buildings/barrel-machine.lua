5ce3ab1b192347bb51941b32383f0e56edb97fbe
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-15 10:18:18 +0200

    remove excess volume

--- prototypes/buildings/barrel-machine.lua
+++ prototypes/buildings/barrel-machine.lua
@@ -104,7 +104,6 @@ ENTITY {
             pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
             pipe_covers = py.pipe_covers(false, true, true, true),
             volume = 1000,
-            volume = 100,
             pipe_connections = {{flow_direction = "output", position = {1.0, 1.3}, direction = defines.direction.south}}
         },
         off_when_no_fluid_recipe = true
