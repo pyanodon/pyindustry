454d64cc884b68ceda6f64968bba89620e3a269e
Lemon <55360995+lemonlambda@users.noreply.github.com>
2024-11-15 20:54:58 -0500

    Fixed quartz tile to not look like concrete (#106)

--- prototypes/tiles/py-quartz.lua
+++ prototypes/tiles/py-quartz.lua
@@ -38,9 +38,13 @@ data.raw.tile["lab-white"].vehicle_friction_modifier = 0.6
 data.raw.tile["lab-white"].tint = {255, 255, 255}
 
 -- giving it concrete base/transitions
-data.raw.tile["lab-white"].variants = table.deepcopy(data.raw.tile.concrete.variants)
-data.raw.tile["lab-white"].variants.main = {{
-    picture = "__base__/graphics/terrain/lab-tiles/lab-white.png",
-    count = 1,
-    size = 1
-}}
+-- data.raw.tile["lab-white"].variants = table.deepcopy(data.raw.tile.concrete.variants)
+data.raw.tile["lab-white"].variants = {
+    transition = table.deepcopy(data.raw.tile["concrete"].variants.transition),
+    main =
+    {{
+        picture = "__base__/graphics/terrain/lab-tiles/lab-white.png",
+        count = 1,
+        size = 1
+    }}
+}
