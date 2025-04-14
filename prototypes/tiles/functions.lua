498f27371852748e4ca0f54137f3a6f3b18d3d1f
Zachary Picco <zacharypicco@gmail.com>
2025-04-13 19:30:41 -0500

    Rename some tiles in pySE

--- prototypes/tiles/functions.lua
+++ prototypes/tiles/functions.lua
@@ -1,5 +1,5 @@
 if mods.pystellarexpedition then
-    table.insert(data.raw.tile["grass-1"].transitions[2].to_tiles, "space")
-    table.insert(data.raw.tile["concrete"].transitions[2].to_tiles, "space")
-    table.insert(data.raw.tile["stone-path"].transitions[2].to_tiles, "space")
+    table.insert(data.raw.tile["grass-1"].transitions[2].to_tiles, "empty-space")
+    table.insert(data.raw.tile["concrete"].transitions[2].to_tiles, "empty-space")
+    table.insert(data.raw.tile["stone-path"].transitions[2].to_tiles, "empty-space")
 end
