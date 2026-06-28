0e0d1a39c52f0b79ab589545d83604cfcc3c6c15
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-06-27 21:29:06 -0700

    for real this time

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -16,11 +16,7 @@ if settings.startup["py-braided-pipes"].value then
         end
 
         for _, entity in pairs(data.raw[entity_prototype] or {}) do
-            local fluid_boxes = {}
-
-            for _, fluid_box in pairs(entity.fluid_boxes or {}) do
-                fluid_boxes[#fluid_boxes + 1] = fluid_box
-            end
+            local fluid_boxes = entity.fluid_boxes or {}
 
             -- add generically, if they're nil this does nothing
             fluid_boxes[#fluid_boxes + 1] = entity.fluid_box
@@ -31,7 +27,7 @@ if settings.startup["py-braided-pipes"].value then
 
             if entity.energy_source and entity.energy_source.type == "fluid" then
                 fluid_boxes[#fluid_boxes + 1] = entity.energy_source.fluid_box
-                fluid_boxes[#fluid_boxes + 1] = entity.energy_source.fluid_box.output_fluid_box
+                fluid_boxes[#fluid_boxes + 1] = entity.energy_source.output_fluid_box
             end
 
             for _, fluid_box in pairs(fluid_boxes) do
