49db9ec00e1e4adc2336e195b42a5b46eceea3f2
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-08-25 00:32:35 -0700

    Fixed misaligned pipe connection graphics. Resolves https://github.com/pyanodon/pybugreports/issues/1540

--- prototypes/buildings/barrel-machine.lua
+++ prototypes/buildings/barrel-machine.lua
@@ -96,14 +96,14 @@ ENTITY {
         --1
         {
             production_type = "input",
-            pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
+            pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.75}, nil, nil),
             pipe_covers = py.pipe_covers(false, true, true, true),
             volume = 1000,
             pipe_connections = {{flow_direction = "input", position = {-1.0, -1.0}, direction = defines.direction.north}}
         },
         {
             production_type = "output",
-            pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.96}, nil, nil),
+            pipe_picture = py.pipe_pictures("assembling-machine-2", nil, {0.0, -0.75}, nil, nil),
             pipe_covers = py.pipe_covers(false, true, true, true),
             volume = 1000,
             pipe_connections = {{flow_direction = "output", position = {1.0, 1.0}, direction = defines.direction.south}}
