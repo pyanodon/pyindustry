d8e351708dc611af0e793c891006c537c58d918e
protocol_1903 <67478786+protocol-1903@users.noreply.github.com>
2026-09-03 18:53:07 -0700

    Changed check valves to be actual valve prototypes. Existing valves will become legacy objects, as new valves cannot connect to the circuit network.

--- prototypes/buildings/valves/check-valve.lua
+++ prototypes/buildings/valves/check-valve.lua
@@ -27,7 +27,9 @@ ITEM {
 
 ENTITY {
     name = "py-check-valve",
-    type = "storage-tank",
+    type = "valve",
+    mode = "one-way",
+    flow_rate = 12000 / 60,
     minable = {mining_time = 1, result = "py-check-valve"},
     icon = "__pyindustrygraphics__/graphics/icons/check-valve.png",
     icon_size = 32,
@@ -38,9 +40,6 @@ ENTITY {
     fast_replaceable_group = data.raw["pipe"]["pipe"].fast_replaceable_group,
     collision_box = data.raw["pipe"]["pipe"].collision_box, --0.29
     selection_box = data.raw["pipe"]["pipe"].selection_box,
-    window_bounding_box = {{0, 0}, {0, 0}},
-    flow_length_in_ticks = data.raw["storage-tank"]["storage-tank"].flow_length_in_ticks,
-    working_sound = nil,
     fluid_box = {
         volume = 100,
         pipe_covers = _G.pipecoverspictures(),
@@ -52,11 +51,120 @@ ENTITY {
             {
                 position = {0, -0},
                 direction = defines.direction.north,
-                -- flow_direction = "output" --TODO fix valves
+                flow_direction = "output"
             }
         }
     },
-    pictures = {
+    animations = {
+        north = {
+            layers = {
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve.png",
+                    priority = "extra-high",
+                    width = 64,
+                    height = 128,
+                    shift = {0.0, -0.5},
+                    scale = 0.5,
+                    x = 64 * 0
+                },
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve-shadow.png",
+                    priority = "extra-high",
+                    draw_as_shadow = true,
+                    width = 96,
+                    height = 48,
+                    shift = util.by_pixel(12, 0),
+                    scale = 0.5,
+                    x = 96 * 0
+                }
+            }
+        },
+        east = {
+            layers = {
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve.png",
+                    priority = "extra-high",
+                    width = 64,
+                    height = 128,
+                    shift = {0.0, -0.5},
+                    scale = 0.5,
+                    x = 64 * 1
+                },
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve-shadow.png",
+                    priority = "extra-high",
+                    draw_as_shadow = true,
+                    width = 96,
+                    height = 48,
+                    shift = util.by_pixel(12, 0),
+                    scale = 0.5,
+                    x = 96 * 1
+                }
+            }
+        },
+        south = {
+            layers = {
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve.png",
+                    priority = "extra-high",
+                    width = 64,
+                    height = 128,
+                    shift = {0.0, -0.5},
+                    scale = 0.5,
+                    x = 64 * 2
+                },
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve-shadow.png",
+                    priority = "extra-high",
+                    draw_as_shadow = true,
+                    width = 96,
+                    height = 48,
+                    shift = util.by_pixel(12, 0),
+                    scale = 0.5,
+                    x = 96 * 0
+                }
+            }
+        },
+        west = {
+            layers = {
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve.png",
+                    priority = "extra-high",
+                    width = 64,
+                    height = 128,
+                    shift = {0.0, -0.5},
+                    scale = 0.5,
+                    x = 64 * 3
+                },
+                {
+                    filename = "__pyindustrygraphics__/graphics/entity/py-valves/check-valve-shadow.png",
+                    priority = "extra-high",
+                    draw_as_shadow = true,
+                    width = 96,
+                    height = 48,
+                    shift = util.by_pixel(12, 0),
+                    scale = 0.5,
+                    x = 96 * 1
+                }
+            }
+        }
+    },
+    -- circuit_connector = circuit_connector_definitions["py-valves"], -- re add at some point WUBE
+    -- circuit_wire_max_distance = data.raw["storage-tank"]["storage-tank"].circuit_wire_max_distance
+}
+
+
+if not mods["pystellarexpedition"] then
+    local legacy_entity = table.deepcopy(data.raw.valve["py-check-valve"])
+    legacy_entity.name = "py-check-valve-legacy"
+    legacy_entity.type = "storage-tank"
+    legacy_entity.circuit_connector = circuit_connector_definitions["py-valves"]
+    legacy_entity.circuit_wire_max_distance = data.raw["storage-tank"]["storage-tank"].circuit_wire_max_distance
+    legacy_entity.window_bounding_box = {{0, 0}, {0, 0}}
+    legacy_entity.flow_length_in_ticks = data.raw["storage-tank"]["storage-tank"].flow_length_in_ticks
+    legacy_entity.fluid_box.pipe_connections[2].flow_direction = nil
+    legacy_entity.placeable_by = {item = "py-check-valve", count = 1}
+    legacy_entity.pictures = {
         gas_flow = py.empty_image(),
         fluid_background = py.empty_image(),
         window_background = py.empty_image(),
@@ -84,7 +192,10 @@ ENTITY {
                 }
             }
         }
-    },
-    circuit_connector = circuit_connector_definitions["py-valves"],
-    circuit_wire_max_distance = data.raw["storage-tank"]["storage-tank"].circuit_wire_max_distance
-}
+    }
+    legacy_entity.localised_name = {"", {"entity-name.py-check-valve"}, " (Legacy)"}
+    legacy_entity.localised_description = {"entity-description.py-check-valve"}
+    legacy_entity.hidden = true
+    legacy_entity.hidden_in_factoriopedia = true
+    data:extend {legacy_entity}
+end
